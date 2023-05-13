3d6aed4406b7b23b04876993592133aaebebe80a
Nosikov Daniil <danilanosikov@yandex.ru>
2023-05-13 20:18:44 +0300

    Initial Commit

---
+++ OkulaR/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OkulaR
+//
+//  Created by Носиков Даниил Григорьевич on 13.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
