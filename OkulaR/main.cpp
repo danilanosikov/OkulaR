a648d4e5aed5c6b53e9b6224bbf6988118ee3f61
Nosikov Daniil <danilanosikov@yandex.ru>
2023-05-15 19:21:55 +0300

    Library Set-Up

--- OkulaR/main.cpp
+++ OkulaR/main.cpp
@@ -1,14 +1,106 @@
-//
-//  main.cpp
-//  OkulaR
-//
-//  Created by Носиков Даниил Григорьевич on 13.05.2023.
-//
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-#include <iostream>
+//#include <iostream>
+//#include <exception>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    
+    GLFWwindow* window;
+    
+    if(!glfwInit()) return -1;
+    
+    window = glfwCreateWindow(600, 600, "Hello World", NULL, NULL);
+    
+    if(!window) { glfwTerminate(); return -1; }
+    
+    
+    // Set-Up Context
+    glfwMakeContextCurrent(window);
+    
+    
+    // Initialise GLEW
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        return -1;
+    }
+    
+    
+    
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Draw;
+        glBegin(GL_TRIANGLES);
+            glVertex2f(-0.5f, -0.5f);
+            glVertex2f(0.0f, 0.5f);
+            glVertex2f(0.5f, -0.5f);
+        glEnd();
+        
+        glfwSwapBuffers(window);
+        
+        glfwPollEvents();
+        
+    }
+    
+    
     return 0;
 }
+
+
+//int main(void)
+//{
+//
+//    #define GL_SILENCE_DEPRECATION
+//
+//    // Apple-specific settings
+//    #ifdef __APPLE__
+//        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+//        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+//        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//    #endif
+//
+//
+//
+//    glfwInit() ?
+//        std::cout << "GLFW initialisation SUCCESS\n" :
+//        throw std::runtime_error("GLFW initialisation FAILURE\n");
+//
+//
+//    // Init Winodw
+//    GLFWwindow* body = glfwCreateWindow(800, 800, "Hello Window", NULL, NULL);
+//    body ?
+//        std::cout<<"Body creation SUCCESS\n" :
+//        throw std::runtime_error("Body creation FAIILURE\n");
+//
+//    // Apply It's Parameters
+//    glfwMakeContextCurrent(body);
+//
+//    // Main Loop
+//    while (!glfwWindowShouldClose(body)){
+//
+//        // Clearance of colour buffer
+//        glClear(GL_COLOR_BUFFER_BIT);
+//
+//        // Draw objects
+//        glBegin(GL_TRIANGLES);
+//            glVertex2f(-0.5f, -0.5f);
+//            glVertex2f(0.0f, 0.5f);
+//            glVertex2f(0.5f, -0.5f);
+//        glEnd();
+//        // Swap back buffer and the front one
+//        glfwSwapBuffers(body);
+//
+//
+//        glfwPollEvents();
+//
+//    }
+//
+//    // Terminate GLFW
+//    glfwTerminate();
+//
+//
+//    return 0;
+//}
